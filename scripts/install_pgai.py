bdc1c9456ce5ce5791c3b08790b58dfe4220da57
Branden <branden@excellenttech.co.za>
2025-12-21 20:24:58 +0200

    init pgai

--- scripts/install_pgai.py
+++ scripts/install_pgai.py
@@ -1,7 +1,7 @@
 import pgai
 
 pgai.install(
-    "postgres://postgres:password@127.0.0.1:5432/postgres"
+    "postgres://postgres:postgres@127.0.0.1:5432/postgres"
 )
 
 print("pgAI installed successfully")
