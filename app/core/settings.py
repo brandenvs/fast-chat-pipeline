58680f05e76cdaa2b6cfbaae99cf14f4ad218520
Branden <branden@excellenttech.co.za>
2026-01-02 23:53:38 +0200

    fix weaviate

--- app/core/settings.py
+++ app/core/settings.py
@@ -11,7 +11,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
 import os
 import weaviate
 
@@ -20,10 +19,16 @@ WEAVIATE_HTTP_PORT = int(os.getenv("WEAVIATE_HTTP_PORT", "8080"))
 WEAVIATE_GRPC_HOST = os.getenv("WEAVIATE_GRPC_HOST", WEAVIATE_HTTP_HOST)
 WEAVIATE_GRPC_PORT = int(os.getenv("WEAVIATE_GRPC_PORT", "50051"))
 
+WEAVIATE_HTTP_SECURE = os.getenv("WEAVIATE_HTTP_SECURE", "false").lower() == "true"
+WEAVIATE_GRPC_SECURE = os.getenv("WEAVIATE_GRPC_SECURE", "false").lower() == "true"
+
 def weaviate_client():
     return weaviate.connect_to_custom(
         http_host=WEAVIATE_HTTP_HOST,
         http_port=WEAVIATE_HTTP_PORT,
+        http_secure=WEAVIATE_HTTP_SECURE,
         grpc_host=WEAVIATE_GRPC_HOST,
         grpc_port=WEAVIATE_GRPC_PORT,
+        grpc_secure=WEAVIATE_GRPC_SECURE,
     )
+
